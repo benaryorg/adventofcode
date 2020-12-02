eb4d93be06c50d2ac4aadb31ba9ab3395efb15f4
benaryorg <binary@benary.org>
2020-12-02 13:18:52 +0100

    1.1
    Signed-off-by: benaryorg <binary@benary.org>

--- src/main.rs
+++ src/main.rs
@@ -34,19 +34,19 @@ fn main() -> Result<()>
 	let headers: reqwest::header::HeaderMap = [(reqwest::header::COOKIE,format!("session={}",std::env!("ADVENTOFCODE_SESSION")).parse().unwrap())].iter().cloned().collect();
 	let http = reqwest::blocking::Client::builder().default_headers(headers).build()?;
 	let body = http.get("https://adventofcode.com/2020/day/1/input").send()?.text()?;
-	let numbers = body.lines().map(|s| Ok(s.parse::<usize>()?)).collect::<Result<Vec<_>>>()?;
+	let numbers = body.lines().map(|s| Ok(s.parse::<usize>()?)).collect::<Result<BTreeSet<_>>>()?;
 	let target = 2020;
-	let (low,high): (BTreeSet<_>,BTreeSet<_>) = numbers.into_iter().partition(|&i| i < target/2);
-	for &low in std::iter::once(&0).chain(low.iter())
+	for &base in numbers.iter()
 	{
-		let result = high.iter()
-			.take_while(|&high| high + low <= target)
-			.find(|&high| high + low == target);
-
-		if let Some(result) = result
+		let target = target - base;
+		for &low in numbers.range(..(target/2))
 		{
-			println!("{}", low.max(1) * result);
-			return Ok(());
+			let target = target - low;
+			if let Some(&high) = numbers.get(&target)
+			{
+				println!("{}", base * low * high);
+				return Ok(());
+			}
 		}
 	}
 
