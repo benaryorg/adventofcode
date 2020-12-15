0c4e8bf7108e78cfdb2045beec414ac0154a80cc
benaryorg <binary@benary.org>
2020-12-15 06:45:09 +0100

    15.0
    Signed-off-by: benaryorg <binary@benary.org>

--- src/main.rs
+++ src/main.rs
@@ -36,73 +36,52 @@ pub mod error
 
 use error::*;
 
-#[derive(Clone,Debug)]
-struct Bus
-{
-	offset: u128,
-	step_size: u128,
-}
-
-impl Bus
-{
-	fn new(offset: u128, id: u128) -> Self
-	{
-		Self
-		{
-			offset,
-			step_size: id,
-		}
-	}
-
-	fn contains(&self, number: u128) -> bool
-	{
-		(0..).step_by(self.step_size as usize).take_while(|&i| i <= number).find(|&i| i == number).is_some()
-	}
-}
-
 fn main() -> Result<()>
 {
 	let timer = std::time::Instant::now();
 
-	let headers: reqwest::header::HeaderMap = [(reqwest::header::COOKIE,format!("session={}",std::env!("ADVENTOFCODE_SESSION")).parse().unwrap())].iter().cloned().collect();
-	let http = reqwest::blocking::Client::builder().default_headers(headers).build()?;
-	let body = http.get("https://adventofcode.com/2020/day/13/input").send()?.text()?;
-	//let body = "foo\n7,13,x,x,59,x,31,19";
+	//let headers: reqwest::header::HeaderMap = [(reqwest::header::COOKIE,format!("session={}",std::env!("ADVENTOFCODE_SESSION")).parse().unwrap())].iter().cloned().collect();
+	//let http = reqwest::blocking::Client::builder().default_headers(headers).build()?;
+	let body = "9,19,1,6,0,5,4";
 
 	println!("fetched in {:.3}s", timer.elapsed().as_secs_f64());
 	let timer = std::time::Instant::now();
 
-	let mut lines = body.lines();
-	let _ = lines.next().ok_or(ErrorKind::ParseError)?;
-	let mut busses = lines.next()
-		.ok_or(ErrorKind::ParseError)?
-		.split(",")
-		.enumerate()
-		.filter(|&(_,id)| id != "x")
-		.map(|(idx,id)| Ok(Bus::new(idx as u128,id.parse()?)))
+	let nums = body.split(",")
+		.map(|num| Ok(num.parse::<usize>()?))
 		.collect::<Result<Vec<_>>>()?;
 
-	busses.sort_by_key(|bus| bus.step_size);
+	let mut map = nums.iter()
+		.copied()
+		.enumerate()
+		.map(|(idx,num)| (num,idx+1))
+		.take(nums.len() - 1)
+		.collect::<std::collections::BTreeMap<_,_>>();
+	
+	let next = *nums.last().ok_or(ErrorKind::NoSolution)?;
+	let mut turn = nums.len();
 
-	let mut step_size = 1;
-	let mut base_time = 1;
-	loop
+	let result = std::iter::successors(Some(next),|&last|
 	{
-		if busses.iter().all(|bus| bus.contains(base_time + bus.offset))
-		{
-			println!("{}", base_time);
-			println!("done in {:.3}s", timer.elapsed().as_secs_f64());
-			return Ok(());
-		}
+		let mut next = 0;
+		map.entry(last)
+			.and_modify(|atime|
+			{
+				next = turn - *atime;
+				*atime = turn;
+			})
+			.or_insert(turn);
+		turn += 1;
 
-		for bus in busses.iter().filter(|bus| bus.contains(base_time + bus.offset))
-		{
-			step_size = num::integer::lcm(bus.step_size,step_size);
-		}
+		Some(next)
+	})
+		.take(2021 - nums.len())
+		.last()
+		.ok_or(ErrorKind::NoSolution)?;
 
-		base_time += step_size;
-	}
+	println!("{}", result);
+	println!("done in {:.3}s", timer.elapsed().as_secs_f64());
 
-	bail!(ErrorKind::NoSolution);
+	Ok(())
 }
 
