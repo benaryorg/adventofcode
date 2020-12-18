6c5bc365c39ab41f06c2478d618080d59c763b19
benaryorg <binary@benary.org>
2020-12-18 18:01:33 +0100

    unify d17pt1
    Signed-off-by: benaryorg <binary@benary.org>

--- src/solution/y2020/mod.rs
+++ src/solution/y2020/mod.rs
@@ -1,9 +1,11 @@
+mod d17pt1;
 mod d17pt2;
 
 pub fn parsers<'a>() -> Vec<Box<dyn super::InputParser<'a>>>
 {
 	vec!
 	[
+		super::helper::coerce_parser_input((2020,17,1),|input| { d17pt1::Solution::new(input.expect("empty input received")) } ),
 		super::helper::coerce_parser_input((2020,17,2),|input| { d17pt2::Solution::new(input.expect("empty input received")) } ),
 	]
 }
