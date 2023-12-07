a7976540243daffdbf0ab874ff2c3daae0aa4a27
benaryorg <binary@benary.org>
2023-12-07 06:38:01 +0000

    2023-12-07 part 1
    Signed-off-by: benaryorg <binary@benary.org>

---
+++ src/solution/y2023/d7pt1.rs
@@ -0,0 +1,238 @@
+use crate::error::*;
+
+use ::
+{
+	std::
+	{
+		convert::TryFrom,
+		cmp::Ordering,
+	},
+};
+
+/// # Examples
+///
+/// ```
+/// # use adventofcode::solution::{ y2023::d7pt1::Solution, Solution as S };
+/// # env_logger::init();
+/// let input = "\
+///     32T3K 765\n\
+///     T55J5 684\n\
+///     KK677 28\n\
+///     KTJJT 220\n\
+///     QQQJA 483";
+/// assert_eq!(Solution::new(input.to_string()).solve().unwrap(), "6440");
+/// ```
+pub struct Solution
+{
+	input: String,
+}
+
+impl Solution
+{
+	pub fn new(input: String) -> Self
+	{
+		Self { input, }
+	}
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+enum Card
+{
+	A,
+	K,
+	Q,
+	J,
+	T,
+	Number(u32),
+}
+
+impl From<Card> for u32
+{
+	fn from(card: Card) -> u32
+	{
+		match card
+		{
+			Card::A => 14,
+			Card::K => 13,
+			Card::Q => 12,
+			Card::J => 11,
+			Card::T => 10,
+			Card::Number(num) => num,
+		}
+	}
+}
+
+impl PartialOrd for Card
+{
+    fn partial_cmp(&self, o: &Self) -> Option<Ordering>
+    {
+		Some(self.cmp(o))
+    }
+}
+
+impl Ord for Card
+{
+    fn cmp(&self, other: &Self) -> Ordering
+    {
+		let s: u32 = self.clone().into();
+		let o: u32 = other.clone().into();
+		s.cmp(&o)
+    }
+}
+
+impl TryFrom<char> for Card
+{
+	type Error = Error;
+	fn try_from(input: char) -> std::result::Result<Self, Error>
+	{
+		match input
+		{
+			'A' => Ok(Card::A),
+			'K' => Ok(Card::K),
+			'Q' => Ok(Card::Q),
+			'J' => Ok(Card::J),
+			'T' => Ok(Card::T),
+			'2'..='9' => Ok(Card::Number(input.to_digit(10).unwrap())),
+			_ => Err(Error::AocParsing),
+		}
+	}
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+enum Set
+{
+	Five(Vec<Card>),
+	Four(Vec<Card>),
+	Full(Vec<Card>),
+	Three(Vec<Card>),
+	TwoPair(Vec<Card>),
+	Two(Vec<Card>),
+	Nothing(Vec<Card>),
+}
+
+impl std::str::FromStr for Set
+{
+	type Err = Error;
+	fn from_str(input: &str) -> std::result::Result<Self, Error>
+	{
+		trace!("card parser input: {:?}", input);
+		if input.len() != 5 { Err(Error::AocParsing)? };
+		let cards = input.chars().map(|ch| Ok(Card::try_from(ch)?)).collect::<Result<Vec<Card>>>()?;
+		trace!("parsing cards: {:?}", cards);
+
+		let map = cards.iter()
+			.cloned()
+			.fold(std::collections::BTreeMap::<Card, usize>::new(), |mut map, card|
+			{
+				*map.entry(card).or_default() += 1;
+				map
+			});
+
+		if let Some((card, _)) = map.iter().rev().find(|&(_, count)| *count == 5)
+		{
+			trace!("found five: {:?}", card);
+			return Ok(Set::Five(cards));
+		}
+		if let (Some((four, _)), Some((one, _))) = (map.iter().rev().find(|&(_, count)| *count == 4), map.iter().rev().find(|&(_, count)| *count == 1))
+		{
+			trace!("found four: {:?} & {:?}", four, one);
+			return Ok(Set::Four(cards));
+		}
+		if let (Some((three, _)), Some((two, _))) = (map.iter().rev().find(|&(_, count)| *count == 3), map.iter().rev().find(|&(_, count)| *count == 2))
+		{
+			trace!("found full house: {:?} & {:?}", three, two);
+			return Ok(Set::Full(cards));
+		}
+		if let Some((three, _)) = map.iter().rev().find(|&(_, count)| *count == 3)
+		{
+			let mut rest = map.iter().rev().filter(|&(_, count)| *count == 1);
+			let o1 = rest.next().unwrap().0;
+			let o2 = rest.next().unwrap().0;
+			trace!("found three: {:?} & {:?} & {:?}", three, o1, o2);
+			return Ok(Set::Three(cards));
+		}
+		if map.iter().rev().filter(|&(_, count)| *count == 2).count() == 2
+		{
+			let mut twos = map.iter().rev().filter(|&(_, count)| *count == 2);
+			let t1 = twos.next().unwrap().0;
+			let t2 = twos.next().unwrap().0;
+			let one = map.iter().rev().find(|&(_, count)| *count == 1).unwrap().0;
+			trace!("found two pairs: {:?} & {:?} & {:?}", t1, t2, one);
+			return Ok(Set::TwoPair(cards));
+		}
+		if let Some((two, _)) = map.iter().rev().find(|&(_, count)| *count == 2)
+		{
+			let mut rest = map.iter().rev().filter(|&(_, count)| *count == 1);
+			let o1 = rest.next().unwrap().0;
+			let o2 = rest.next().unwrap().0;
+			let o3 = rest.next().unwrap().0;
+			trace!("found pair: {:?} & {:?} & {:?} & {:?}", two, o1, o2, o3);
+			return Ok(Set::Two(cards));
+		}
+		trace!("found pair: {:?}", cards);
+		Ok(Set::Nothing(cards))
+	}
+}
+
+impl PartialOrd for Set
+{
+    fn partial_cmp(&self, o: &Self) -> Option<Ordering>
+    {
+		Some(self.cmp(o))
+    }
+}
+
+impl Ord for Set
+{
+    fn cmp(&self, other: &Self) -> Ordering
+    {
+		match (self, other)
+		{
+			(Set::Five(l), Set::Five(r)) => l.cmp(r),
+			(Set::Five(_), _) => Ordering::Greater,
+			(_, Set::Five(_)) => Ordering::Less,
+			(Set::Four(l), Set::Four(r)) => l.cmp(r),
+			(Set::Four(_), _) => Ordering::Greater,
+			(_, Set::Four(_)) => Ordering::Less,
+			(Set::Full(l), Set::Full(r)) => l.cmp(r),
+			(Set::Full(_), _) => Ordering::Greater,
+			(_, Set::Full(_)) => Ordering::Less,
+			(Set::Three(l), Set::Three(r)) => l.cmp(r),
+			(Set::Three(_), _) => Ordering::Greater,
+			(_, Set::Three(_)) => Ordering::Less,
+			(Set::TwoPair(l), Set::TwoPair(r)) => l.cmp(r),
+			(Set::TwoPair(_), _) => Ordering::Greater,
+			(_, Set::TwoPair(_)) => Ordering::Less,
+			(Set::Two(l), Set::Two(r)) => l.cmp(r),
+			(Set::Two(_), _) => Ordering::Greater,
+			(_, Set::Two(_)) => Ordering::Less,
+			(Set::Nothing(l), Set::Nothing(r)) => l.cmp(r),
+		}
+    }
+}
+
+impl super::super::Solution for Solution
+{
+	fn solve(&self) -> Result<String>
+	{
+		debug!("called with input: {}", self.input);
+
+		let players = self.input.lines()
+			.map(|line|
+			{
+				let (cards, score) = line.split_once(' ').ok_or(Error::AocParsing)?;
+				let cards = cards.parse::<Set>().context(Error::AocParsing)?;
+				let score = score.parse::<usize>().context(Error::AocParsing)?;
+				Ok((cards, score))
+			})
+			.collect::<Result<std::collections::BTreeSet<(Set, usize)>>>()?;
+
+		let result: usize = players.into_iter().enumerate()
+			.inspect(|(idx, (set, score))| trace!("rank {}: {:?} (score: {})", idx + 1, set, score))
+			.map(|(idx, (_, score))| score * (idx + 1))
+			.sum();
+
+		Ok(format!("{}", result))
+	}
+}
+
--- src/solution/y2023/mod.rs
+++ src/solution/y2023/mod.rs
@@ -10,6 +10,7 @@ pub mod d5pt1;
 pub mod d5pt2;
 pub mod d6pt1;
 pub mod d6pt2;
+pub mod d7pt1;
 
 pub fn parsers<'a>() -> Vec<Box<dyn super::InputParser<'a>>>
 {
@@ -34,6 +35,7 @@ pub fn parsers<'a>() -> Vec<Box<dyn super::InputParser<'a>>>
 		Some(InputType::UrlInput(Box::new(|input| Box::new(d5pt2::Solution::new(input.expect("empty input received")))))),
 		Some(InputType::UrlInput(Box::new(|input| Box::new(d6pt1::Solution::new(input.expect("empty input received")))))),
 		Some(InputType::UrlInput(Box::new(|input| Box::new(d6pt2::Solution::new(input.expect("empty input received")))))),
+		Some(InputType::UrlInput(Box::new(|input| Box::new(d7pt1::Solution::new(input.expect("empty input received")))))),
 	];
 
 	days.into_iter()
