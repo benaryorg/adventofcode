1fb21034c13de33b729b7a345c18640da547b2f0
benaryorg <binary@benary.org>
2023-12-25 18:31:26 +0000

    2023-12-25 no hardcoding
    Signed-off-by: benaryorg <binary@benary.org>

--- src/solution/y2023/d25.rs
+++ src/solution/y2023/d25.rs
@@ -1,12 +1,12 @@
 use crate::error::*;
 
+use std::ops::Bound;
+
+/// May not work for all inputs.
+///
 /// # Examples
 ///
-/// Cannot run the test because the values in the code are hard-coded for my solution.
-/// Use *neato* on the dot trace output to get a graph of your stuff, open it up in a browser or something, look at the graph and determine the three values, then run the modified code.
-/// I'm sorry, but this problem is NP-hard and any solution for estimating requires complex math and/or libraries.
-///
-/// ```no_run
+/// ```
 /// # use adventofcode::solution::{ y2023::d25::Solution, Solution as S };
 /// # env_logger::init();
 /// let input = "\
@@ -62,6 +62,39 @@ fn count_cluster<T>(origin: T, connections: &std::collections::BTreeSet<(T, T)>)
 	}
 }
 
+fn walk<T>(dist: &mut std::collections::BTreeMap<T, usize>, connections: &std::collections::BTreeSet<(T, T)>) -> usize
+	where
+		T: Ord + Copy,
+{
+	let min = connections.first().unwrap().0;
+	let max = connections.last().unwrap().0;
+	let current = dist.values().max().unwrap() + 1;
+
+	let last_round = dist.iter()
+		.filter_map(|(&k, &v)| (v == current - 1).then_some(k))
+		.collect::<Vec<_>>();
+
+	let next = last_round.iter()
+		.flat_map(|&key|
+		{
+			connections.range((Bound::Included((key, min)), Bound::Included((key, max))))
+				.map(|(_, b)| b)
+		})
+		.filter(|next| !dist.contains_key(next))
+		.map(|&next| (next, current))
+		.collect::<Vec<_>>();
+
+	if next.is_empty()
+	{
+		return current - 1;
+	}
+	else
+	{
+		dist.extend(next);
+		walk(dist, connections)
+	}
+}
+
 impl super::super::Solution for Solution
 {
 	fn solve(&self) -> Result<String>
@@ -72,7 +105,7 @@ impl super::super::Solution for Solution
 			.map(|line|
 			{
 				let (left, right) = line.split_once(": ").ok_or(anyhow!("cannot split on colon"))?;
-				Ok(right.split(' ').map(move |right| (left.min(right), left.max(right))))
+				Ok(right.split(' ').flat_map(move |right| [(left, right), (right, left)]))
 			})
 			.collect::<Result<Vec<_>>>()?
 			.into_iter()
@@ -86,11 +119,44 @@ impl super::super::Solution for Solution
 			trace!("dot data:\ngraph {{\n{}\n}}", repr.join("\n"));
 		}
 
-		conns.remove(&("cms", "thk"));
-		conns.remove(&("dht", "xmv"));
-		conns.remove(&("rcn", "xkf"));
+		let nodes = conns.iter().flat_map(|&(a, b)| [a, b]).collect::<std::collections::BTreeSet<_>>();
+		let min_node = *nodes.first().unwrap();
+		let max_node = *nodes.last().unwrap();
+		let dist = nodes.iter()
+			.map(|&n|
+			{
+				(walk(&mut std::collections::BTreeMap::from([(n, 0)]), &conns), n)
+			})
+			.inspect(|(dist, n)| trace!("{:?}: max distance: {}", n, dist))
+			.collect::<std::collections::BTreeSet<_>>();
+		let dist = &dist;
 
-		let result: usize = count_cluster("cms", &conns) * count_cluster("thk", &conns);
+		let min_dist = dist.first().unwrap().0;
+		let min_count = dist.range((Bound::Included((min_dist, min_node)), Bound::Included((min_dist, max_node)))).count();
+		let remove = dist.range((Bound::Included((min_dist, min_node)), Bound::Included((min_dist, max_node))))
+			.map(|&(d, n)|
+			{
+				let next = conns.range((Bound::Included((n, min_node)), Bound::Included((n, max_node))))
+					.map(|(_, b)| b)
+					.filter_map(|n|
+					{
+						dist.iter().find(|(od, on)| n == on && (min_count == 6 || *od > min_dist))
+					})
+					.min_by_key(|(od, _)| od)
+					.unwrap()
+					.1;
+				debug!("shortest dist ({}) from {:?}: {:?}", d, n, next);
+				(n, next)
+			})
+			.collect::<Vec<_>>();
+
+		for (a, b) in remove.iter()
+		{
+			conns.remove(&(a, b));
+			conns.remove(&(b, a));
+		}
+
+		let result: usize = count_cluster(remove[0].0, &conns) * count_cluster(remove[0].1, &conns);
 
 		Ok(format!("{}", result))
 	}
